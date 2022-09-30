6ad56e7456aab0cf6b53a5234d281c0fe1fae435
Gustavo <gustavo.borges.guimaraes100@gmail.com>
2022-09-30 16:22:37 -0300

    Implementando resource e service. (incompleto)

---
+++ src/main/java/me/dio/sacola/resource/SacolaResource.java
@@ -0,0 +1,32 @@
+package me.dio.sacola.resource;
+
+import lombok.RequiredArgsConstructor;
+import me.dio.sacola.model.Item;
+import me.dio.sacola.model.Sacola;
+import me.dio.sacola.service.SacolaService;
+import me.dio.sacola.service.impl.SacolaServiceImpl;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/ifood-devweek/sacolas")
+public class SacolaResource {
+
+    private final SacolaService sacolaService;
+
+    @PostMapping
+    public Item incluirItemNaSacola(@RequestBody ItemDto itemDto) {
+        return sacolaService.incluirItemNaSacola(itemDto);
+    }
+
+    @GetMapping("/{id}")
+    public Sacola verSacola(@PathVariable("id") Long id) {
+        return sacolaService.verSacola(id);
+    }
+
+    @PatchMapping("/fecharSacola/{sacolaId}")
+    public Sacola fecharSacola(@PathVariable("sacolaId") Long sacolaId, @RequestParam("formaPagamento") int formaPagamento) {
+        return sacolaService.fecharSacola(sacolaId, formaPagamento);
+    }
+
+}
